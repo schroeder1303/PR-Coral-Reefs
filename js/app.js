92dc3655aef70757553d14023065824354573c5c
schroeder1303 <103450989+schroeder1303@users.noreply.github.com>
2023-05-04 18:55:53 -0400

    Update app.js
    updated app.js with coral reef triage data
    papaparse
    legend
    popup

--- js/app.js
+++ js/app.js
@@ -1,70 +1,101 @@
-// map options
 var options = {
     center: [18.2208, -66.5901],
     zoom: 9
-}
+  }
 
-// create a Leaflet map in our division container with id of 'map'
-var map = L.map('map', options);
+  // create a Leaflet map in our division container with id of 'map'
+  var map = L.map('map', options);
 
-// Leaflet providers base map URL
-var basemap_source =
+  // Leaflet providers base map URL
+  var basemap_source =
     'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
 
-// Leaflet providers attributes
-var basemap_options = {
+  // Leaflet providers attributes
+  var basemap_options = {
     attribution: '&copy; <a href="&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
+  };
+
+  // request some basemap tiles and add to the map
+  var tiles = L.tileLayer(basemap_source, basemap_options).addTo(map);
+
+// Load the CSV file using AJAX
+fetch('/data/PR-Maria.csv')
+.then(function (response) {
+  // Parse the CSV into a useable format, then return it
+  return response.text();
+})
+.then(function (csv) {
+  // Parse the CSV data into a JavaScript object
+  var data = Papa.parse(csv, { header: true, dynamicTyping: false }).data;
+  // Loop through each row in the data and draw markers
+  data.forEach(function (row) {
+    drawMap(row);
+  });
+})
+.catch(function (error) {
+  console.log(error);
+});
+//.then(function () {
+//  addLegend();
+//});
+
+function drawMap(row) {
+  var siteDamage = row.SiteDamage;
+  var siteName = row.SiteName;
+  var restorePotential = row.RestorePotential;
+
+
+  // Create a circle marker based on the SiteDamage value
+  var color, radius;
+  if (siteDamage === "Minor") {
+    color = "yellow";
+    radius = 8;
+  } else if (siteDamage === "Moderate") {
+    color = "orange";
+    radius = 8;
+  } else if (siteDamage === "Severe") {
+    color = "red";
+    radius = 8;
+  }
+
+  // If a circle marker color was determined, create the marker and add it to the map
+  if (color) {
+    var circleMarker = L.circleMarker([row.Latitude, row.Longitude], {
+      radius: radius,
+      color: color,
+      fillColor: color,
+      fillOpacity: 0.8
+    });
+
+    circleMarker.addTo(map);
+    // Add a tooltip popup to the circle marker
+    circleMarker.bindPopup("<b>" + "Reef Name:" + siteName + "</b><br>" + 
+                          "<br>" + "Restoration Potential: " + restorePotential);
+  }
 };
 
-// request some basemap tiles and add to the map
-var tiles = L.tileLayer(basemap_source, basemap_options).addTo(map);
+// Add legend to map
+function addLegend() {
+// create a new Leaflet control object, and position it top right
+const legendControl = L.control({ position: "topright" });
 
+// when the legend is added to the map
+legendControl.onAdd = function () {
+  const legend = L.DomUtil.create("div", "legend");
+  legend.innerHTML += '<div><span style="background-color: yellow"></span>Minor</div>';
+  legend.innerHTML += '<div><span style="background-color: orange"></span>Moderate</div>';
+  legend.innerHTML += '<div><span style="background-color: red"></span>Severe</div>';
+  
+  // add a colored circle next to each legend item
+  const legendItems = legend.querySelectorAll("div");
+  legendItems.forEach(function(item) {
+    const color = item.querySelector("span").style.backgroundColor;
+    item.innerHTML = '<span style="display: inline-block; margin-left: 8px; width: 10px; height: 10px; border-radius: 50%; margin-right: 6px; background-color: ' + color + '"></span>' + item.innerHTML;
+  });
 
-fetch("data/reef.geojson")
-      // after it is returned...
-      .then(function (response) {
-        // Parse the JSON into a useable format, then return it
-        return response.json();
-      })
-      // The returned response is now data in a new then method
-      .then(function (data) {
-        // This is the JSON from our response
-        console.log(data);
-        drawMap(data);
-           
-      })
-      // If there is an error, log it to the console
-      .catch(function (error) {
-        console.log(error);
-      });
-
-
-
-function drawMap(data) {
-    const reefs = L.geoJson(data, {
-        style: function (feature) {
-            var threat = feature.properties.threat;
-            var color;
-            switch (threat) {
-                case 0:
-                    color = '#fff5f0';
-                    break;
-                case 100:
-                    color = '#fca486';
-                    break;
-                case 1000:
-                    color = '#67000d';
-                    break;
-                default:
-                    color = 'gray';
-            }
-            return { color: color };
-        }
-    }).addTo(map);
+  return legend;
+};
+legendControl.addTo(map);
 }
-
-
-
-
